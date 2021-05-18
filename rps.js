4e7fdf098992ed95fd3052faca8129d5a92e53ab
Parmvir-S <parmvir.shergill@gmail.com>
2021-05-17 21:16:04 -0700

    Basic Layout

---
+++ rps.js
@@ -0,0 +1,11 @@
+//Get Past Start Page
+function getGameDisplay() {
+    let screen = document.querySelector(".load-screen");
+    let disappear = document.querySelector(".disappear");
+    disappear.classList.remove("disappear");
+    screen.classList.add("disappear");
+
+}
+window.addEventListener("click", getGameDisplay);
+window.addEventListener("keydown", getGameDisplay);
+
